0b334e7f8c72d797a2d0a5c4406e9b17b41cafb4
Pat Wey <patrickwey@gmail.com>
2015-09-01 15:31:48 -0600

    added tests for list accepting a node and includes? method

--- test/list_test.rb
+++ test/list_test.rb
@@ -1,8 +1,25 @@
+$LOAD_PATH << __dir__ # delete this eventually
+
 require 'minitest'
 require 'list'
+require 'node'
 
 class ListTest < Minitest::Test
   def test_head_defaults_to_nil
     assert_equal nil, List.new.head
+  end
+
+  def test_list_accepts_node_as_head
+    node = Node.new('HEAD')
+    list = List.new(node)
+    assert_equal node, list.head
+  end
+
+  def test_includes_returns_true_if_value_is_in_list
+    list = List.new
+    node_a = Node.new('a')
+    list.head = node_a
+
+    assert list.includes?('a')
   end
 end
