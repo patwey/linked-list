7c4019f02708467fc70b4f1056c79b190680799c
Pat Wey <patrickwey@gmail.com>
2015-09-01 17:40:14 -0600

    refactored includes?, added append and prepend

--- lib/list.rb
+++ lib/list.rb
@@ -9,23 +9,41 @@ class List
     @head = head
   end
 
-  def includes?(data)
-    node = head
+  def append(new_node)
+    self.head = new_node if self.head.nil?
+    current_node = head
     loop do
-      return true if node.data = data
-      next_node = node.next_node
-      break if next_node.nil?
+      if current_node.next_node.nil?
+        current_node.next_node = new_node
+        break
+      else
+        current_node = current_node.next_node
+      end
+    end
+  end
+
+  def prepend(new_node)
+    new_node.next_node = self.head
+    self.head = new_node
+  end
+
+  def includes?(data)
+    current_node = head
+    loop do
+      return true if current_node.data == data
+      return false if current_node.next_node.nil?
+      current_node = current_node.next_node
     end
   end
 end
 
 # Linked List
+
+
 linked_list = List.new
 node_a = Node.new('a')
 node_b = Node.new('b')
 
-linked_list.head = node_a
-node_a.next_node = node_b
-
-node_c = Node.new('c')
-node_b.next_node = node_c
+linked_list.append(node_a)
+linked_list.head
+linked_list
