83fe2ce39c78cef9402c09c5bd58176a2eede3d8
Pat Wey <patrickwey@gmail.com>
2015-09-01 14:44:50 -0600

    reworded list tests

--- test/list_test.rb
+++ test/list_test.rb
@@ -2,7 +2,7 @@ require 'minitest'
 require 'list'
 
 class ListTest < Minitest::Test
-  def test_empty_list_has_nil_head
+  def test_head_defaults_to_nil
     assert_equal nil, List.new.head
   end
 end
