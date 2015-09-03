098f1c9271bf89b29c02748f7be4b2cd66233438
Pat Wey <patrickwey@gmail.com>
2015-09-03 00:02:33 -0600

    refactored list class, consolidated tests

--- lib/list.rb
+++ lib/list.rb
@@ -11,106 +11,102 @@ class List
 
   def append(new_node)
     self.head = new_node if self.head.nil?
-    current_node = head
-    loop do
-      if current_node.next_node.nil?
-        current_node.next_node = new_node
-        break
-      else
-        current_node = current_node.next_node
-      end
-    end
+    tail.link = new_node
   end
 
   def prepend(new_node)
-    new_node.next_node = self.head
+    new_node.link = self.head
     self.head = new_node
   end
 
-  def includes?(data)
-    current_node = head
-    loop do
-      return true if current_node.data == data
-      return false if current_node.next_node.nil?
-      current_node = current_node.next_node
-    end
+  def remove_head(head_node)
+    self.head = nil
+    head_node
   end
 
-  def pop
-    return if self.head.nil?
-    current_node = head
-    if current_node.next_node.nil?
-      self.head = nil
-      return current_node
-    else
-      previous_node = current_node
-      next_node = current_node.next_node
-    end
-
-    while current_node.next_node
-      previous_node = current_node
-      current_node = current_node.next_node
-    end
-
-    previous_node.next_node = nil
-    current_node
+  def increment_nodes(prev, current)
+    prev = current
+    current = current.link
+    return prev, current
   end
 
-  def count
-    return 0 if self.head.nil?
-    return 1 if self.head.next_node.nil?
-
-    count = 1
-    current_node = head
-
-    while current_node.next_node
-      current_node = current_node.next_node
+  def count_nodes(current, count)
+    while current.link
+      current = current.link
       count += 1
     end
     count
   end
 
+  def find_pre_tail
+    prev = head
+    while prev.link != tail
+      prev = prev.link
+    end
+    prev
+  end
+
+  def node_at_index(index)
+    current = head
+    index.times do
+      current.link ? current = current.link : return
+    end
+    current
+  end
+
+  def node_at_value(value, current)
+    while current.link
+      return current if current.data == value
+      return current.link if current.link.data == value
+      current = current.link
+    end
+  end
+
+  def includes?(data)
+    current_node = self.head
+    loop do
+      return true if current_node.data == data
+      return false if current_node.link.nil?
+      current_node = current_node.link
+    end
+  end
+
+  def pop
+    current = self.head
+    return if current.nil?
+    return remove_head(current) if !current.link
+    prev = find_pre_tail
+    tail = prev.link
+    prev.link = nil
+    tail
+  end
+
+  def count
+    return 0 if !self.head
+    return 1 if !self.head.link
+    count_nodes(head, 1)
+  end
+
   def tail
     return if self.head.nil?
-    current_node = head
-    while current_node.next_node
-      current_node = current_node.next_node
+    current = self.head
+    while current.link
+      current = current.link
     end
-    current_node
+    current
   end
 
   def find_by_index(index)
-    return if self.head.nil?
+    return if !self.head
     return self.head if index == 0
-    current_node = head
-    index.times do
-      if current_node.next_node
-        current_node = current_node.next_node
-      else
-        return
-      end
-    end
-    current_node
+    node_at_index(index)
   end
 
   def find_by_value(value)
-    return if self.head.nil?
-    current_node = head
-
-    if current_node.data == value
-      return current_node
-    end
-
-    # add a next_node variable?
-    while current_node.next_node
-      if current_node.data == value
-        return current_node
-      elsif current_node.next_node.data == value
-        return current_node.next_node
-      else
-        current_node = current_node.next_node
-      end
-    end
+    current = head
+    return if current.nil?
+    return current if current.data == value
+    node_at_value(value, current)
   end
 
   def remove_by_index(index)
@@ -118,19 +114,19 @@ class List
       self.head = nil
     elsif index == 1
       previous_node = self.head
-      current_node = previous_node.next_node
-      previous_node.next_node = current_node.next_node
+      current_node = previous_node.link
+      previous_node.link = current_node.link
     else
-      current_node = self.head.next_node
+      current_node = self.head.link
       previous_node = head
 
       (index - 1).times do
-        if current_node.next_node
+        if current_node.link
           previous_node = current_node
-          current_node = current_node.next_node
+          current_node = current_node.link
         end
       end
-      previous_node.next_node = current_node.next_node
+      previous_node.link = current_node.link
     end
   end
 
@@ -138,20 +134,20 @@ class List
     return if self.head.nil?
     current_node = head
     if current_node.data == value
-      next_node = current_node.next_node
+      next_node = current_node.link
       self.head = next_node
     else
-      if current_node.next_node
+      if current_node.link
         previous_node = current_node
-        current_node = current_node.next_node
-        while current_node.next_node
+        current_node = current_node.link
+        while current_node.link
           if current_node.data == value
-            next_node = current_node.next_node
-            previous_node.next_node = next_node
+            next_node = current_node.link
+            previous_node.link = next_node
             return
           else
             previous_node = current_node
-            current_node = current_node.next_node
+            current_node = current_node.link
           end
         end
       end
@@ -164,43 +160,58 @@ class List
     current_node = head
     includes_a = false
     includes_b = false
-    while current_node.next_node
+
+    while current_node.link
       if current_node == node_a ||
-         current_node.next_node == node_a
+         current_node.link == node_a
         includes_a = true
       end
       if current_node == node_b ||
-            current_node.next_node == node_b
+         current_node.link == node_b
         includes_b = true
       end
-      current_node = current_node.next_node
+      current_node = current_node.link
     end
 
     if includes_a && includes_b
       distance = 0
+      measure_distance = false
       current_node = head
 
       while current_node != node_b
         if current_node == node_a
+          measure_distance = true
+        end
+          current_node = current_node.link
+        if measure_distance == true
           distance += 1
-          current_node = current_node.next_node
-        else
-          current_node = current_node.next_node
         end
       end
 
       if distance == 0
+        measure_distance == false
+
         while current_node != node_a
           if current_node == node_b
+            measure_distance = true
+          end
+
+          current_node = current_node.link
+
+          if measure_distance == true
             distance += 1
-            current_node = current_node.next_node
-          else
-            current_node = current_node.next_node
           end
         end
       end
     end
     distance
   end
-
 end
+
+list = List.new
+node = Node.new
+node2 = Node.new
+
+node.link = node2
+list.head = node
+list.pop
--- test/list_test.rb
+++ test/list_test.rb
@@ -5,411 +5,289 @@ require 'list'
 require 'node'
 
 class ListTest < Minitest::Test
+  def setup
+    @list = List.new
+    @node = Node.new
+    @node_b = Node.new
+    @node_c = Node.new
+    @node_d = Node.new
+    @node_e = Node.new
+    @node_f = Node.new
+  end
+
   def test_head_defaults_to_nil
-    assert_nil List.new.head
+    assert_nil @list.head
   end
 
   def test_list_accepts_node_as_head
-    node = Node.new('HEAD')
-    list = List.new(node)
+    @list.head = @node
 
-    assert_equal node, list.head
-  end
-
-  def test_includes_returns_true_if_value_in_list
-    node_a = Node.new('a')
-    list = List.new(node_a)
-
-    node_b = Node.new('b')
-    node_a.next_node = node_b
-
-    assert list.includes?('b')
+    assert_equal @node, @list.head
   end
 
   def test_includes_returns_false_if_value_not_in_list
-    node_a = Node.new('a')
-    list = List.new(node_a)
+    @node.data = 'a'
+    @list.head = @node
 
-    refute list.includes?('b')
+    refute @list.includes?('b')
   end
 
-  def test_includes_returns_true_if_value_in_head
-    head_node = Node.new('a')
-    list = List.new(head_node)
+  def test_includes_true_if_value_in_head
+    @node.data = 'a'
+    @list.head = @node
 
-    assert list.includes?('a')
+    assert @list.includes?('a')
   end
 
-  def test_includes_returns_true_if_value_in_tail
-    head_node = Node.new('head')
-    list = List.new(head_node)
+  def test_includes_true_if_value_in_middle
+    @node.data = 'a'
+    @node_b.data = 'b'
+    @node_c.data = 'c'
 
-    tail_node = Node.new('tail')
-    head_node.next_node = tail_node
+    @node.link = @node_b
+    @node_b.link = @node_c
 
-    assert list.includes?('tail')
+    @list.head = @node
+
+    assert @list.includes?('b')
+  end
+
+  def test_includes_true_if_value_in_tail
+    @node.data = 'a'
+    @node_b.data = 'b'
+
+    @node.link = @node_b
+    @list.head = @node
+
+    assert @list.includes?('b')
   end
 
   def test_append_adds_node_to_head_if_list_is_empty
-    list = List.new
-    node = Node.new
-    list.append(node)
+    @list.append(@node)
 
-    assert_equal node, list.head
+    assert_equal @node, @list.head
   end
 
   def test_append_adds_node_to_end_of_list
-    node_a = Node.new('a')
-    list = List.new(node_a)
+    @list.head = @node
+    @list.append(@node_b)
 
-    node_b = Node.new('b')
-    list.append(node_b)
-    tail = list.head.next_node
-
-    assert_equal node_b, tail
+    assert_equal @node_b, @node.link
   end
 
   def test_prepend_points_head_to_node_if_list_empty
-    list = List.new
-    node = Node.new
+    @list.prepend(@node)
 
-    list.prepend(node)
-
-    assert_equal node, list.head
+    assert_equal @node, @list.head
   end
 
-  def test_prepend_sets_head_to_new_node_if_list_has_head
-    node_a = Node.new
-    list = List.new(node_a)
+  def test_prepend_inserts_new_node_at_head
+    @list.head = @node
+    @list.prepend(@node_b)
 
-    new_node = Node.new
-    list.prepend(new_node)
-
-    assert_equal new_node, list.head
-  end
-
-  def test_prepend_links_new_node_to_previous_head
-    node_a = Node.new
-    list = List.new(node_a)
-
-    new_node = Node.new
-    list.prepend(new_node)
-
-    assert_equal node_a, new_node.next_node
-  end
-
-  def test_pop_returns_head_with_one_node
-    node_a = Node.new
-    list = List.new(node_a)
-
-    new_node = Node.new
-    list.pop
-
-    assert_nil list.head
-  end
-
-  def test_pop_returns_last_with_two_nodes
-    node_a = Node.new
-    node_b = Node.new
-    node_a.next_node = node_b
-
-    list = List.new(node_a)
-    assert_equal node_b, list.pop
+    assert_equal @node_b, @list.head
+    assert_equal @node, @node_b.link
   end
 
   def test_pop_returns_nil_with_empty_list
-    list = List.new
-    assert_nil list.pop
+    assert_nil @list.pop
   end
 
-  def test_pop_returns_last_with_six_nodes
-    node_f = Node.new
-    node_e = Node.new('', node_f)
-    node_d = Node.new('', node_e)
-    node_c = Node.new('', node_d)
-    node_b = Node.new('', node_c)
-    node_a = Node.new('', node_b)
+  def test_pop_returns_head_with_one_node
+    @list.head = @node
 
-    list = List.new(node_a)
-    assert_equal node_f, list.pop
+    assert_equal @node, @list.pop
+    assert_nil @list.head
   end
 
-  def test_count_is_zero_with_empty_list
-    list = List.new
-    assert_equal 0, list.count
+  def test_pop_returns_tail
+    @node.link = @node_b
+    @list.head = @node
+
+    assert_equal @node_b, @list.pop
+
+    @node.link = @node_b
+    @node_b.link = @node_c
+    @node_c.link = @node_d
+    @node_d.link = @node_e
+
+    @list.head = @node
+
+    assert_equal @node_e, @list.pop
   end
 
-  def test_count_counts_one_node
-    list = List.new(Node.new)
-    assert_equal 1, list.count
+  def test_count_returns_number_of_nodes_in_list
+    assert_equal 0, @list.count
+
+    @list.head = @node
+    assert_equal 1, @list.count
+
+    @node.link = @node_b
+    assert_equal 2, @list.count
+
+    @node.link = @node_b
+    @node_b.link = @node_c
+    @node_c.link = @node_d
+    @node_d.link = @node_e
+    @list.head = @node
+
+    assert_equal 5, @list.count
   end
 
-  def test_count_counts_two_nodes
-    node_a = Node.new
-    node_b = Node.new
-    node_a.next_node = node_b
+  def test_head_returns_list_head
+    assert_nil @list.head
 
-    list = List.new(node_a)
-
-    assert_equal 2, list.count
+    @list = List.new(@node)
+    assert_equal @node, @list.head
   end
 
-  def test_count_counts_six_nodes
-    node_f = Node.new
-    node_e = Node.new('', node_f)
-    node_d = Node.new('', node_e)
-    node_c = Node.new('', node_d)
-    node_b = Node.new('', node_c)
-    node_a = Node.new('', node_b)
+  def test_tail_returns_last_node
+    assert_nil @list.tail
 
-    list = List.new(node_a)
+    @node.link = @node_b
+    @list.head = @node
 
-    assert_equal 6, list.count
+    assert_equal @node_b, @list.tail
   end
 
-  def test_head_returns_nil_with_empty_list
-    list = List.new
-    assert_nil list.head
-  end
+  def test_find_by_index_returns_node_at_index
+    assert_nil @list.find_by_index(0)
 
-  def test_head_returns_first_node
-    node = Node.new
-    list = List.new(node)
+    @list.head = @node
+    assert_nil @list.find_by_index(1)
 
-    assert_equal node, list.head
-  end
+    @list.head = @node
+    assert_equal @list.head, @list.find_by_index(0)
 
-  def test_tail_returns_nil_with_empty_list
-    list = List.new
-    assert_nil list.tail
-  end
+    @node.link = @node_b
+    @list.head = @node
+    assert_equal @node_b, @list.find_by_index(1)
 
-  def test_tail_returns_last_node_with_two_nodes
-    node_b = Node.new
-    node_a = Node.new('', node_b)
-
-    list = List.new(node_a)
-
-    assert_equal node_b, list.tail
-  end
-
-  def test_find_by_index_returns_nil_with_empty_list
-    list = List.new
-
-    assert_nil list.find_by_index(0)
-  end
-
-  def test_find_by_index_nil_if_index_doesnt_exist
-    node = Node.new
-    list = List.new(node)
-
-    assert_nil list.find_by_index(1)
-  end
-
-  def test_find_by_index_zero_returns_head
-    node = Node.new
-    list = List.new(node)
-
-    assert_equal list.head, list.find_by_index(0)
-  end
-
-  def test_find_by_index_one_returns_second_node
-    node_b = Node.new
-    node_a = Node.new('', node_b)
-    list = List.new(node_a)
-
-    assert_equal node_b, list.find_by_index(1)
-  end
-
-  def test_find_by_index_five_returns_sixth_node
-    node_f = Node.new
-    node_e = Node.new('', node_f)
-    node_d = Node.new('', node_e)
-    node_c = Node.new('', node_d)
-    node_b = Node.new('', node_c)
-    node_a = Node.new('', node_b)
-
-    list = List.new(node_a)
-
-    assert_equal node_f, list.find_by_index(5)
-  end
-
-  def test_find_by_value_returns_nil_with_empty_list
-    list = List.new
-    assert_nil list.find_by_value('a')
-  end
-
-  def test_find_by_value_returns_nil_if_value_doesnt_exist
-    node_b = Node.new('b')
-    node_a = Node.new('a', node_b)
-
-    list = List.new
-
-    assert_nil list.find_by_value('c')
-  end
-
-  def test_find_by_value_returns_head_if_it_holds_value
-    node_a = Node.new('a')
-    list = List.new(node_a)
-
-    assert_equal node_a, list.find_by_value('a')
-  end
-
-  def test_find_by_value_returns_tail_if_it_holds_value
-    node_b = Node.new('b')
-    node_a = Node.new('a', node_b)
-    list = List.new(node_a)
-
-    assert_equal node_b, list.find_by_value('b')
+    @node.link = @node_b
+    @node_b.link = @node_c
+    @node_c.link = @node_d
+    @node_d.link = @node_e
+    @list.head = @node
+    assert_equal @node_e, @list.find_by_index(4)
   end
 
   def test_find_by_value_returns_first_node_with_value
-    node_b = Node.new('a')
-    node_a = Node.new('a', node_b)
-    list = List.new(node_a)
+    assert_nil @list.find_by_value('a')
 
-    assert_equal node_a, list.find_by_value('a')
+    @node.data = 'a'
+    @list.head = @node
+    assert_nil @list.find_by_value('b')
+
+    @node.data = 'a'
+    @list.head = @node
+    assert_equal @node, @list.find_by_value('a')
+
+    @node.link = @node_b
+    @node_b.data = 'b'
+    @list.head = @node
+    assert_equal @node_b, @list.find_by_value('b')
+
+    @node.data = 'a'
+    @node.link = @node_b
+    @node_b.data = 'a'
+    @list.head = @node
+    assert_equal @node, @list.find_by_value('a')
   end
 
-  def test_remove_by_index_returns_nil_with_empty_list
-    list = List.new
-    assert_nil list.remove_by_index(0)
+  def test_remove_by_index_removes_node_at_index
+    assert_nil @list.remove_by_index(0)
+
+    @list.head = @node
+    @list.remove_by_index(0)
+    assert_nil @list.head
+
+    @node.link = @node_b
+    @list.head = @node
+    @list.remove_by_index(1)
+    assert_nil @node.link
   end
 
-  def test_remove_by_index_zero_removes_head
-    node = Node.new
-    list = List.new(node)
+  def test_remove_by_index_one_links_adjacent_nodes
+    @node.link = @node_b
+    @node_b.link = @node_c
 
-    list.remove_by_index(0)
+    @list.head = @node
+    @list.remove_by_index(1)
+    assert_equal @node_c, @node.link
 
-    assert_nil list.head
+    @node.link = @node_b
+    @node_b.link = @node_c
+    @node_c.link = @node_d
+
+    @list.head = @node
+    @list.remove_by_index(2)
+    assert_equal @node_d, @node_b.link
   end
 
-  def test_remove_by_index_one_removes_second_node
-    node_b = Node.new
-    node_a = Node.new('', node_b)
-    list = List.new(node_a)
+  def test_remove_by_value_removes_node_with_value
+    assert_nil @list.remove_by_value('a')
 
-    list.remove_by_index(1)
-    node_a_link = node_a.next_node
-
-    assert_nil node_a_link
+    @node.data = 'a'
+    @list.head = @node
+    @list.remove_by_value('a')
+    assert_nil @list.head
   end
 
-  def test_remove_by_index_one_links_head_with_two
-    node_c = Node.new
-    node_b = Node.new('', node_c)
-    node_a = Node.new('', node_b)
-    list = List.new(node_a)
+  def test_remove_by_value_removes_first_node_with_value
+    @node.data = 'a'
+    @node_b.data = 'a'
+    @node.link = @node_b
 
-    list.remove_by_index(1)
-    node_a_link = node_a.next_node
+    @list.head = @node
+    @list.remove_by_value('a')
 
-    assert_equal node_c, node_a_link
-  end
-
-  def test_remove_by_index_two_links_one_with_three
-    node_d = Node.new
-    node_c = Node.new('', node_d)
-    node_b = Node.new('', node_c)
-    node_a = Node.new('', node_b)
-
-    list = List.new(node_a)
-
-    list.remove_by_index(2)
-    node_b_link = node_b.next_node
-
-    assert_equal node_d, node_b_link
-  end
-
-  def test_remove_by_index_four_links_node_three_with_five
-    node_f = Node.new
-    node_e = Node.new('', node_f)
-    node_d = Node.new('', node_e)
-    node_c = Node.new('', node_d)
-    node_b = Node.new('', node_c)
-    node_a = Node.new('', node_b)
-
-    list = List.new(node_a)
-
-    list.remove_by_index(4)
-    node_d_link = node_d.next_node
-
-    assert_equal node_f, node_d_link
-  end
-
-  def test_remove_by_value_returns_nil_with_empty_list
-    list = List.new
-    assert_nil list.remove_by_value('a')
-  end
-
-  def test_remove_by_value_removes_head_if_it_holds_value
-    node_a = Node.new('a')
-    list = List.new(node_a)
-
-    list.remove_by_value('a')
-
-    assert_nil list.head
-  end
-
-  def test_remove_by_value_returns_first_node_holding_value
-    node_b = Node.new('a')
-    node_a = Node.new('a', node_b)
-    list = List.new(node_a)
-
-    list.remove_by_value('a')
-
-    assert_equal node_b, list.head
+    assert_equal @node_b, @list.head
   end
 
   def test_remove_by_value_links_adjecent_nodes
-    node_c = Node.new
-    node_b = Node.new('b', node_c)
-    node_a = Node.new('a', node_b)
+    @node.data = 'a'
+    @node_b.data = 'b'
+    @node_c.data = 'c'
 
-    list = List.new(node_a)
-    list.remove_by_value('b')
-    node_a_link = node_a.next_node
+    @node.link = @node_b
+    @node_b.link = @node_c
 
-    assert_equal node_c, node_a_link
-  end
+    @list.head = @node
+    @list.remove_by_value('b')
 
-  def test_find_distance_returns_nil_with_empty_list
-    list = List.new
-    node_a = Node.new
-    node_b = Node.new
-
-    assert_nil list.find_distance(node_a, node_b)
-  end
-
-  def test_find_distance_returns_nil_if_node_isnt_in_list
-    node_b = Node.new
-    node_a = Node.new('', node_b)
-    list = List.new(node_a)
-
-    assert_nil list.find_distance(node_a, Node.new)
-    assert_nil list.find_distance(Node.new, node_b)
-  end
-
-  def test_find_distance_returns_zero_if_passed_the_same_node
-    node_a = Node.new
-    list = List.new(node_a)
-    assert_equal 0, list.find_distance(node_a, node_a)
+    assert_equal @node_c, @node.link
   end
 
   def test_find_distance_returns_distance_between_two_nodes
-    node_b = Node.new
-    node_a = Node.new('', node_b)
-    list = List.new(node_a)
+    assert_nil @list.find_distance(@node_a, @node_b)
 
-    assert_equal 1, list.find_distance(node_a, node_b)
-  end
+    @list.head = @node
 
-  def test_find_distance_returns_distance_if_nodes_are_reversed
-    node_b = Node.new
-    node_a = Node.new('', node_b)
-    list = List.new(node_a)
+    assert_nil @list.find_distance(@node, Node.new)
+    assert_nil @list.find_distance(Node.new, @node)
 
-    assert_equal 1, list.find_distance(node_b, node_a)
+    @list.head = @node
+
+    assert_equal 0, @list.find_distance(@node, @node)
+
+    @node.link = @node_b
+    @list.head = @node
+    distance = @list.find_distance(@node, @node_b)
+
+    assert_equal 1, distance
+
+    @node.link = @node_b
+    @list.head = @node
+
+    assert_equal 1, @list.find_distance(@node_b, @node)
+
+    @node.link = @node_b
+    @node_b.link = @node_c
+    @node_c.link = @node_d
+    @node_d.link = @node_e
+
+    @list.head = @node
+
+    assert_equal 4, @list.find_distance(@node, @node_e)
   end
 end
